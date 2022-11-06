9745441fc0a4eb25054bec90f08a92c066f74c02
Himangshu Mondal <mailtoherock@gmail.com>
2022-11-06 15:49:28 +0530

    ui: button component update

--- components/button/buttonsolid.component.tsx
+++ components/button/buttonsolid.component.tsx
@@ -6,7 +6,7 @@ type SolidButtonProps = {
 const SolidButton = ({ children }: SolidButtonProps) => {
   return (
     <Link href='#'>
-      <a className='bg-primary text-white py-4 px-10 rounded-full inline-block text-base uppercase font-semibold -tracking-wide'>
+      <a className='bg-primary text-white py-5 px-10 rounded-full inline-block text-base uppercase font-semibold -tracking-wide'>
         {children}
       </a>
     </Link>
--- components/button/outlinebutton.component.tsx
+++ components/button/outlinebutton.component.tsx
@@ -6,7 +6,7 @@ type OutLineButtonProps = {
 const OutlineButton = ({ children }: OutLineButtonProps) => {
   return (
     <Link href='#'>
-      <a className='border border-primary text-primary py-4 px-10 rounded-full inline-block text-base uppercase font-semibold -tracking-wide'>
+      <a className='border border-primary text-primary py-5 px-10 rounded-full inline-block text-base uppercase font-semibold -tracking-wide'>
         {children}
       </a>
     </Link>
