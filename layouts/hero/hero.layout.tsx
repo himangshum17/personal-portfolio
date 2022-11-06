81107033373719193fc4e1e6e122970a63543f3d
Himangshu Mondal <mailtoherock@gmail.com>
2022-11-06 15:50:20 +0530

    ui: hero layout update

--- layouts/hero/hero.layout.tsx
+++ layouts/hero/hero.layout.tsx
@@ -6,10 +6,27 @@ const Hero = () => {
     <section className='py-20'>
       <div className='container'>
         <h1 className='text-[80px] leading-tight -tracking-wider pr-40 text-customgray'>
-          I build <strong className='font-black'>applications</strong> that are{' '}
-          <strong className='font-black'>user-friendly</strong>,{' '}
-          <strong className='font-black'>simple</strong> and{' '}
-          <strong className='font-black'>easy to use</strong>.
+          I build{' '}
+          <strong className='font-black relative'>
+            applications
+            <div className='absolute left-0 bg-green-200 h-6 -z-10 w-full bottom-2' />
+          </strong>{' '}
+          that are{' '}
+          <strong className='font-black relative'>
+            user-friendly
+            <div className='absolute left-0 bg-blue-200 h-6 -z-10 w-full bottom-2' />
+          </strong>
+          ,{' '}
+          <strong className='font-black relative'>
+            simple
+            <div className='absolute left-0 bg-pink-200 h-6 -z-10 w-full bottom-2' />
+          </strong>{' '}
+          and{' '}
+          <strong className='font-black relative'>
+            easy to use
+            <div className='absolute left-0 bg-yellow-200 h-6 -z-10 w-full bottom-2' />
+          </strong>
+          .
         </h1>
         <div className='grid grid-cols-2 gap-10'>
           <div className='mt-16'>
@@ -25,8 +42,10 @@ const Hero = () => {
                 Swadesh Softwares .
               </a>
             </p>
-            <Button variant='solid'>solid button</Button>
-            <Button variant='outline'>outline button</Button>
+            <div className='flex gap-4 mt-12'>
+              <Button variant='solid'>view my work</Button>
+              <Button variant='outline'>view my github</Button>
+            </div>
           </div>
           <div className='-mt-12'>
             <Image
