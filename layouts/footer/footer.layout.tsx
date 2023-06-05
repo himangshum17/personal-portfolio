730dc5214d7b552dabfa318e8cc5f6197eb31900
Himangshu Mondal <mailtoherock@gmail.com>
2023-06-06 00:55:57 +0530

    ui: footer ui update with getfullyear utils function

--- layouts/footer/footer.layout.tsx
+++ layouts/footer/footer.layout.tsx
@@ -1,7 +1,21 @@
+import { getYear } from '../../utils/getYear';
+
 const Footer = () => {
+  const fullYear = getYear();
   return (
-    <footer>
-      <div className='container'>Footer</div>
+    <footer className='pt-20' id='footer'>
+      <div className='container border-t dark:border-gray-800 py-8'>
+        <div className='flex gap-3 flex-wrap justify-between'>
+          <p className='text-sm text-gray-600 dark:text-gray-500 font-medium'>
+            &copy; Himangshu Mondal &bull; All Rights Reserved &bull; {fullYear}
+          </p>
+          <a
+            href='mailto:mailtoherock@gmail.com'
+            className='text-sm text-gray-600 dark:text-gray-500 font-medium'>
+            💌 : mailtoherock@gmail.com
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
