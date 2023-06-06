a9f3dd81d8a790d5da38cf2706138a33cf9d7137
Himangshu Mondal <mailtoherock@gmail.com>
2023-06-06 15:51:32 +0530

    ui: projects heading update

--- layouts/projects/projects.layout.tsx
+++ layouts/projects/projects.layout.tsx
@@ -4,8 +4,8 @@ const Projects = () => {
   return (
     <section>
       <div className='container'>
-        <h2 className='text-3xl xl:text-5xl font-semibold -tracking-wider max-w-3xl leading-snug'>
-          Projects that i have been working for the past couple of years
+        <h2 className='text-3xl xl:text-5xl font-semibold -tracking-wider max-w-4xl leading-snug'>
+          Projects that i had a chance to work on for the past couple of years.
         </h2>
         <ProjectsLists />
       </div>
